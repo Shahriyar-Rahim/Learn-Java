ff1323a1b657e51c5d14397ce5de5918a292e3b3
Md. Shahriyar Rahim <rahimislam420.ri35@gmail.com>
2026-07-13 01:27:25 +0600

    if else

---
+++ Day_3/IfElse.java
@@ -0,0 +1,22 @@
+public class IfElse{
+      /*
+       1. Simple if
+       2. if-else
+       3. if-else-if
+       4. Nested if
+      */
+
+       public static void main(String[] args){
+            // If statement
+            // if (condition) {
+            //    execution
+            // }
+
+            int a = 5;
+            if( a % 2 == 0){
+                  System.out.println("Even");
+            } else 
+                  System.out.println("Odd");
+            
+       }
+}
