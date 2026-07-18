00e2a8dcd2c71fec011e6ceb2795d9a6885e7edb
Md. Shahriyar Rahim <rahimislam420.ri35@gmail.com>
2026-07-19 00:13:31 +0600

    return typw

---
+++ Day_5/Return.java
@@ -0,0 +1,12 @@
+public class Return{
+      static int rval(int x) {
+            int a = 5;
+            int sum = a + x;
+            
+            return sum;
+      }
+
+      public static void main(String[] args) {
+            System.out.println(rval(10));
+      }
+}
