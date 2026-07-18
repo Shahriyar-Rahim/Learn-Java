fd8df32bb448a09f3aec15b9aa0f36a0e152afb4
Md. Shahriyar Rahim <rahimislam420.ri35@gmail.com>
2026-07-18 23:58:41 +0600

    parameterised method

---
+++ Day_5/parameters.java
@@ -0,0 +1,9 @@
+public class parameters {
+      static void params(String x) {
+            System.out.println(x + " Hello");
+      }
+
+      public static void main(String[] args) {
+            params("Shahriyar");
+      }
+}
