0a3855aa6dc631054bdcf8ba177c593c5e6a9cc7
Md. Shahriyar Rahim <rahimislam420.ri35@gmail.com>
2026-07-26 22:12:33 +0600

    OOP day2

---
+++ Day_8/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+      private String name = "kfdg";
+      private int age = 2;
+
+      public String getName() {
+            return name;
+      }
+}
---
+++ Day_8/Public.java
@@ -0,0 +1,7 @@
+public class Public{
+      public static void main(String[] args) {
+            Main m = new Main();
+
+            System.out.println(m.getName());
+      }
+}
