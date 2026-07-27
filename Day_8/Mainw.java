82ef28f91d72203430690a6b5f77a18a24d3d2ae
Md. Shahriyar Rahim <rahimislam420.ri35@gmail.com>
2026-07-27 13:21:47 +0600

    i/o scanner

---
+++ Day_8/Mainw.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Mainw {
+       public static void main(String[] args) {
+            Scanner myob = new Scanner(System.in);
+            System.out.println("Enter name");
+
+            String name = myob.nextLine();
+            System.out.println("Name is: " + name);
+       }
+}
